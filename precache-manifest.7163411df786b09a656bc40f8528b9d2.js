3501be2c68ca99dc484b5d20514e33977f23a60c
Ponseelan A <ponseelan.a@pogeyan.com>
2019-10-06 16:02:11 +0530

    Updates

---
+++ precache-manifest.7163411df786b09a656bc40f8528b9d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d965c9f66b3fb0fb8d77697a06d447c",
+    "url": "/tweets-search/index.html"
+  },
+  {
+    "revision": "2a1c47cc078a1e0dc596",
+    "url": "/tweets-search/static/css/main.4249fb2b.chunk.css"
+  },
+  {
+    "revision": "9595d2f52a2e19ec1ba6",
+    "url": "/tweets-search/static/js/2.96965d6d.chunk.js"
+  },
+  {
+    "revision": "2a1c47cc078a1e0dc596",
+    "url": "/tweets-search/static/js/main.6b9cb3f6.chunk.js"
+  },
+  {
+    "revision": "22666f0b77dc5d41429e",
+    "url": "/tweets-search/static/js/runtime-main.b8ca1c00.js"
+  }
+]);
